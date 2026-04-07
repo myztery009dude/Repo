d3ba72e284b69f9871ff21bb27de6948566274a5
myztery009dude <s252221@students.aie.edu.au>
2026-04-07 18:01:21 +0930

    fix le assets

---
+++ Rube Shooter/Assets/scripts/player/player movement.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    public float JumpHeight = 2f;
+    public float fallGravityMultiplier = 2f;
+    public float mouseSensitivity = 2.0f;
+    public float pitchRange = 60.0f;
+
+    private float forwardInputValue;
+    private float strafeInputValue;
+    private bool jumpInput;
+
+    private float terminalVelocity = 53f;
+    private float verticalVelocity;
+
+    private float rotateCameraPitch;
+
+    private Camera firstPersonCam;
+    private CharacterController characterController;
+
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        firstPersonCam = GetComponentInChildren<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        forwardInputValue = Input.GetAxisRaw("Vertical");
+        strafeInputValue = Input.GetAxisRaw("Horizontal");
+        jumpInput = Input.GetButtonDown("Jump");
+        Movement();
+        JumpAndGravity();
+        CameraMovement();
+    }
+
+    void Movement()
+    {
+        Vector3 direction = (transform.forward * forwardInputValue + transform.right * strafeInputValue).normalized * movementSpeed * Time.deltaTime;
+
+        direction += Vector3.up * verticalVelocity * Time.deltaTime;
+
+        characterController.Move(direction);
+    }
+
+    void JumpAndGravity()
+    {
+        if (characterController.isGrounded)
+        {
+            if (verticalVelocity < 0.0f)
+            {
+                verticalVelocity = -2f;
+            }
+
+            if(jumpInput)
+            {
+                verticalVelocity = Mathf.Sqrt(JumpHeight * -2f * Physics.gravity.y);
+            }
+        }
+        else
+        {
+            if(verticalVelocity < terminalVelocity)
+            {
+                float gravityMultiplier = 1;
+                if(characterController.velocity.y < -1)
+                {
+                    gravityMultiplier = fallGravityMultiplier;
+                }
+                verticalVelocity += Physics.gravity.y * gravityMultiplier * Time.deltaTime;
+            }
+        }
+    }
+
+    void CameraMovement()
+    {
+        float rotateYaw = Input.GetAxis("Mouse X") * mouseSensitivity;
+        transform.Rotate(0, rotateYaw, 0);
+
+        rotateCameraPitch += -Input.GetAxis("Mouse Y") * mouseSensitivity;
+        rotateCameraPitch = Mathf.Clamp(rotateCameraPitch, -pitchRange, pitchRange);
+        firstPersonCam.transform.localRotation = Quaternion.Euler(rotateCameraPitch, 0, 0);
+
+    }
+}
